f004c2f6923fa9fba3e22711e365191a988414d2
Matteo <m.cusini@tudelft.nl>
2015-11-09 17:03:32 +0100

    Capillary Pressure Function added

---
+++ shared/FluidProperties/ComputePc.m
@@ -0,0 +1,42 @@
+%Compute Capillary Pressure
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%Matteo Cusini's Research Code
+%Author: Matteo Cusini
+%TU Delft
+%Year: 2015
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function [Pc, dPc] = ComputePc(S, Fluid)
+%Compute capillary pressure
+%   Given the wetting phase saturation it returns Pc and dPc
+switch(Fluid.Pc)
+    case('polynomial')
+        %Define parameters
+        a=300;
+        b=10;
+        c=350;
+        d=40;
+        e=100;
+        %Compute Pc and dPc analytically
+        Pc = a + b*(1-S) + c*(1-S).^2 + d*(1-S).^3 +e*(1-S).^4;
+        dPc = - b - 2*(1-S) - d*3*(1-S).^2 + e*4*(1-S).^3;
+    case('BrooksCorey')
+        %Define parameters
+        Pct = 700;
+        lambda = 2.5;
+        %Compute Pc and dPc analytically
+        Pc = Pct.*((1-Fluid.swc)./(S-Fluid.swc)).^(1/lambda);
+        dPc = 1;
+    case('BentsenAnli')
+        %Define parameters
+        Pct = 700;
+        Pcs = 350;
+        %Compute Pc and dPc analytically
+        Pc = Pct - Pcs.*log(S-Fluid.swc/1-Fluid.swc);
+        dPc = 1;
+end
+% figure(125);
+% plot(S, Pc);
+% xlabel('water saturation');
+% ylabel('Pcow [Pa]');
+end
+
--- shared/FluidProperties/Derivative.m
+++ shared/FluidProperties/Derivative.m
@@ -5,18 +5,18 @@
 %TU Delft
 %Year: 2015
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-function Df=Derivative(s, Fluid)
+function Df = Derivative(s, Fluid)
 %dfdS
-%This function computes the derivative of the fractional flow function with
-%respect to S at a given saturation value.
+%   This function computes the derivative of the fractional flow function with
+%   respect to S at a given saturation value.
 S = (s-Fluid.swc)/(1-Fluid.swc-Fluid.sor); % Rescale saturations
-    swc=Fluid.swc;
-    sor=Fluid.sor;
-    muw=Fluid.muw;
-    muo=Fluid.muo;
-if (strcmp(Fluid.RelPerm,'Quadratic')==1)
-    Df=-(1-swc-sor).^(-1).*((2*muw*muo.*(S-1).*S)./(muw*(S-1).^2+muo.*S.^2).^2);
+swc = Fluid.swc;
+sor = Fluid.sor;
+muw = Fluid.muw;
+muo = Fluid.muo;
+if (strcmp(Fluid.RelPerm,'Quadratic') == 1)
+    Df = -(1-swc-sor).^(-1).*((2*muw*muo.*(S-1).*S)./(muw*(S-1).^2+muo.*S.^2).^2);
 else
-    Df=(1-swc-sor).^(-1)*(muw*muo)./(muw.*(S-1)-muo.*S).^2;
+    Df = (1-swc-sor).^(-1)*(muw*muo)./(muw.*(S-1)-muo.*S).^2;
 end
 end
