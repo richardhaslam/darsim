aea4d1c2634a36963995c7d59504d308a847a292
Matteo <m.cusini@tudelft.nl>
2016-09-29 15:28:52 +0200

    added BO components

---
+++ Fluid/Components/BO_gas_component.m
@@ -0,0 +1,16 @@
+% BO gas component
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%DARSim 2 Reservoir Simulator
+%Author: Matteo Cusini
+%TU Delft
+%Created: 28 September 2016
+%Last modified: 28 September 2016
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+classdef BO_gas_component < component
+    properties
+        rho = 100;
+    end
+    methods
+        
+    end    
+end
---
+++ Fluid/Components/BO_oil_component.m
@@ -0,0 +1,16 @@
+% BO gas phase class
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%DARSim 2 Reservoir Simulator
+%Author: Matteo Cusini
+%TU Delft
+%Created: 28 September 2016
+%Last modified: 28 September 2016
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+classdef BO_oil_component < component
+    properties
+        rho = 800;
+    end
+    methods
+       
+    end    
+end
