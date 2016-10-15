38037748aa3ca2ae05518e65fe15b1cd49763a51
Matteo <m.cusini@tudelft.nl>
2016-10-15 16:57:08 +0200

    ADM system builder

---
+++ Solvers/NonLinearSolvers/SystemBuilders/fim_system_builder_ADM.m
@@ -0,0 +1,26 @@
+%  FIM System Builder for ADM
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%DARSim 2 Reservoir Simulator
+%Author: Matteo Cusini
+%TU Delft
+%Created: 14 October 2016
+%Last modified: 14 October 2016
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+classdef fim_system_builder_ADM < fim_system_builder
+    properties
+ 
+    end
+    methods
+        function UpdateState(obj, delta, ProductionSystem, Formulation, FluidModel, DiscretizationModel)
+            % Update Reservoir State
+            %Formulation.UpdatePandS(delta, ProductionSystem.Reservoir.State);
+            
+            % Solve FS equilibrium
+            %Formulation.UpdateCompositions(ProductionSystem.Reservoir.State, FluidModel, DiscretizationModel.ReservoirGrid.N);
+            % Update Reservoir State
+            Formulation.UpdateState(delta, ProductionSystem.Reservoir.State, FluidModel);
+            % UpdateWells
+            ProductionSystem.Wells.UpdateState(ProductionSystem.Reservoir, FluidModel);
+        end
+    end
+end
