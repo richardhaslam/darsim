26decbe1e6d6afd20254c36ec278f2344fed5c96
Matteo <m.cusini@tudelft.nl>
2017-08-30 18:13:03 +0200

    F-ADM working fine with decoupled bf.

--- ADMOperators/bf_updater_FAMS.m
+++ ADMOperators/bf_updater_FAMS.m
@@ -63,7 +63,7 @@ classdef bf_updater_FAMS < bf_updater_ms
                 End_f = Start_f + Nf - 1;
                 End_c = Start_c + Grid(m).N - 1 ;
                 obj.Amedia{m} = P(Start_f:End_f, Start_c:End_c)' * obj.Amedia{m} * P(Start_f:End_f, Start_c:End_c);
-                obj.TransformIntoTPFA(obj.Amedia{m}, Grid(m));
+                obj.Amedia{m} = obj.TransformIntoTPFA(obj.Amedia{m}, Grid(m));
                 Start_f = End_f + 1;
                 Start_c = End_c + 1;
             end
