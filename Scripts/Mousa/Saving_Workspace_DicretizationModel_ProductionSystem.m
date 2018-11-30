80a216b454b5770beb50daad4278bc84a538e05c
'Mousa' <'S.HosseiniMehr@tudelft.nl'>
2018-11-30 15:14:06 +0100

    Added Mousa's scripts + minor changes to FracGen 3D (ensure 3 cells in each frac).

---
+++ Scripts/Mousa/Saving_Workspace_DicretizationModel_ProductionSystem.m
@@ -0,0 +1,13 @@
+% DiscretizationModel = SinglePhase.Simulation.DiscretizationModel;
+% DiscretizationModel.CoarseGrid = [];
+% DiscretizationModel.OperatorsHandler = [];
+% DiscretizationModel.FineGrid = [];
+% DiscretizationModel.GridMapper = [];
+% ProductionSystem = SinglePhase.Simulation.ProductionSystem;
+
+DiscretizationModel = ImmHomo.Simulation.DiscretizationModel;
+DiscretizationModel.CoarseGrid = [];
+DiscretizationModel.OperatorsHandler = [];
+DiscretizationModel.FineGrid = [];
+DiscretizationModel.GridMapper = [];
+ProductionSystem = ImmHomo.Simulation.ProductionSystem;
