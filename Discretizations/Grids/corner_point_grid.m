6e4562534c2ffce9dd7ffdd1a205c3353b9485f0
Mousa HosseiniMehr <mousa.hosseinimehr@gmail.com>
2020-02-28 13:45:03 +0100

    Added some functions for gravity and depth.

--- Discretizations/Grids/corner_point_grid.m
+++ Discretizations/Grids/corner_point_grid.m
@@ -11,11 +11,17 @@ classdef corner_point_grid < grid_darsim
         CornerPointGridData
         Trans
         HeatTrans
+        Nx
+        Ny
+        Nz
     end
     methods
         function obj = corner_point_grid(ReservoirProperties)
             obj.CornerPointGridData = ReservoirProperties.CornerPointGridData;
-            obj.N = ReservoirProperties.Grid.N_ActiveCells;
+            obj.Nx = ReservoirProperties.Grid.N(1);
+            obj.Ny = ReservoirProperties.Grid.N(2);
+            obj.Nz = ReservoirProperties.Grid.N(3);
+            obj.N = ReservoirProperties.Grid.N_ActiveCells; % For CornerPointGrid, this is the number of cells actively involved, not dot_prod(Nx,Ny,Nz)
             obj.Active = ones(obj.N, 1);
             obj.ActiveTime = ones(obj.N, 1);
             obj.Trans = zeros(ReservoirProperties.CornerPointGridData.N_InternalFaces,1);
@@ -26,15 +32,26 @@ classdef corner_point_grid < grid_darsim
         end
         function ComputeRockTransmissibilities(obj)
             Perm = obj.CornerPointGridData.Permeability;
-            CellNeighbor1Index = obj.CornerPointGridData.CellNeighbor1Index;
-            CellNeighbor2Index = obj.CornerPointGridData.CellNeighbor2Index;
-            CellNeighbor1Vec = obj.CornerPointGridData.CellNeighbor1Vec;
-            CellNeighbor2Vec = obj.CornerPointGridData.CellNeighbor2Vec;
-            Nvec = obj.CornerPointGridData.Nvec;
+            CellNeighbor1Index = obj.CornerPointGridData.Internal_Face.CellNeighbor1Index;
+            CellNeighbor2Index = obj.CornerPointGridData.Internal_Face.CellNeighbor2Index;
+            CellNeighbor1Vec = obj.CornerPointGridData.Internal_Face.CellNeighbor1Vec;
+            CellNeighbor2Vec = obj.CornerPointGridData.Internal_Face.CellNeighbor2Vec;
+            Nvec = obj.CornerPointGridData.Internal_Face.Nvec;
             Trans_Half_1 = sum( Perm(CellNeighbor1Index,1) .* CellNeighbor1Vec .* Nvec , 2 ) ./ sum( CellNeighbor1Vec .* CellNeighbor1Vec , 2 );
             Trans_Half_2 = sum( Perm(CellNeighbor2Index,1) .* CellNeighbor2Vec .* Nvec , 2 ) ./ sum( CellNeighbor2Vec .* CellNeighbor2Vec , 2 );
             obj.Trans = Trans_Half_1 .* Trans_Half_2 ./ (Trans_Half_1 + Trans_Half_2);
         end
-        
+        function CorrectTransmissibilitiesForpEDFM(obj)
+            % Virtual Call
+        end
+        function AddGridCoordinates(obj)
+            obj.GridCoords = [obj.CornerPointGridData.Cell.NW_Top_Corner, obj.CornerPointGridData.Cell.NE_Top_Corner, ...
+                              obj.CornerPointGridData.Cell.SW_Top_Corner, obj.CornerPointGridData.Cell.SE_Top_Corner, ...
+                              obj.CornerPointGridData.Cell.NW_Bot_Corner, obj.CornerPointGridData.Cell.NE_Bot_Corner, ...
+                              obj.CornerPointGridData.Cell.SW_Bot_Corner, obj.CornerPointGridData.Cell.SE_Bot_Corner];
+        end
+        function ComputeDepth(obj, alpha, Thickness)
+            obj.Depth = obj.CornerPointGridData.Cell.Centroid(:,3);
+        end
     end
 end
