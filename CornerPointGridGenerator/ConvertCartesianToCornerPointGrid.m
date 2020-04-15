e6664695830d3b2e617ed0441af4a2ee736831c7
Mousa <mousa.hosseinimehr@gmail.com>
2020-04-15 14:08:30 +0200

    Adding a new script to generate a CornerPointGrid data from Cartesian test case. It is still empty.

---
+++ CornerPointGridGenerator/ConvertCartesianToCornerPointGrid.m
@@ -0,0 +1,8 @@
+%% ConvertCartesianToCornerPointGrid for DARSim
+% This script receives the geometry input from a Cartesian domain and
+% creates CornerPointGrid input data file (txt) for DARSim simulator.
+% 
+% Author: Mousa HosseiniMehr
+% Modified on: 2020/04/15
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% Reading the input file of DARSim
