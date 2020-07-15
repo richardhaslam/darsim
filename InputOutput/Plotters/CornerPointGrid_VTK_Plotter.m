9c59fc786c27a26cc4ff93d0d85818e9a826fb22
Mousa_HosseiniMehr <mousa.hosseinimehr@gmail.com>
2020-07-15 14:48:00 +0200

    Added plotting of the transmissibilities. Improved the VTK plotting. For plotting reservoir grid cells, VTK data type 12 (VTK_HEXAHEDRON) is used. For plotting transmissibilities, VTK data type 9 (VTK_QUAD) is used.

--- InputOutput/Plotters/CornerPointGrid_VTK_Plotter.m
+++ InputOutput/Plotters/CornerPointGrid_VTK_Plotter.m
@@ -28,20 +28,20 @@ classdef CornerPointGrid_VTK_Plotter < VTK_Plotter
             fprintf(fileID, '\n');
             
             fprintf(fileID, ['CELLS ' num2str(Grid.N) ' ' num2str(Grid.N*(1+8)) '\n']);
-            CellCorners = [ Grid.CornerPointGridData.Cell.NW_Top_Corner , ...
-                            Grid.CornerPointGridData.Cell.NE_Top_Corner , ...
+            CellCorners = [ Grid.CornerPointGridData.Cell.SW_Bot_Corner , ...
+                            Grid.CornerPointGridData.Cell.SE_Bot_Corner , ...
+                            Grid.CornerPointGridData.Cell.NE_Bot_Corner , ...
+                            Grid.CornerPointGridData.Cell.NW_Bot_Corner , ...
                             Grid.CornerPointGridData.Cell.SW_Top_Corner , ...
                             Grid.CornerPointGridData.Cell.SE_Top_Corner , ...
-                            Grid.CornerPointGridData.Cell.NW_Bot_Corner , ...
-                            Grid.CornerPointGridData.Cell.NE_Bot_Corner , ...
-                            Grid.CornerPointGridData.Cell.SW_Bot_Corner , ...
-                            Grid.CornerPointGridData.Cell.SE_Bot_Corner ];
+                            Grid.CornerPointGridData.Cell.NE_Top_Corner , ...
+                            Grid.CornerPointGridData.Cell.NW_Top_Corner , ];
             indMatrix = horzcat(8*ones(Grid.N,1) , CellCorners-1 );
             fprintf(fileID, '%d %d %d %d %d %d %d %d %d\n',indMatrix');
             fprintf(fileID, '\n');
             
             fprintf(fileID, ['CELL_TYPES ' num2str(Grid.N) '\n']);
-            fprintf(fileID, '%d ', 11*ones(1,Grid.N));
+            fprintf(fileID, '%d ', 12*ones(1,Grid.N));
             fprintf(fileID, '\n\n');
             
             % Print all existing variables
@@ -65,6 +65,55 @@ classdef CornerPointGrid_VTK_Plotter < VTK_Plotter
             obj.PrintScalar2VTK(fileID, Grid.Active, ' ACTIVEFine');
             fprintf(fileID, '\n');
             
+            fclose(fileID);
+        end
+        function PlotInternalFaces(obj, Reservoir, Grid)
+            %Write a VTK file for Reservoir
+            fileID = fopen(strcat(obj.FileName, '_Interfaces', num2str(obj.VTKindex),'.vtk'), 'w');
+            fprintf(fileID, '# vtk DataFile Version 2.0\n');
+            fprintf(fileID, 'DARSim 2 Reservoir Simulator\n');
+            if obj.isBinary
+                fprintf(fileID, 'BINARY\n');
+            else
+            	fprintf(fileID, 'ASCII\n');
+            end
+            fprintf(fileID, '\n');
+            fprintf(fileID, 'DATASET UNSTRUCTURED_GRID\n');
+            
+            fprintf(fileID, ['POINTS ' num2str(Grid.CornerPointGridData.N_Nodes) ' double\n']);
+            fprintf(fileID, '%f %f %f\n',Grid.CornerPointGridData.Nodes_XYZ_Coordinate');
+            fprintf(fileID, '\n');
+            
+            N_InternalFaces = Grid.CornerPointGridData.N_InternalFaces;
+            CellCorners = cell2mat( Grid.CornerPointGridData.Internal_Face.Corners );
+            N_NodePerFace = size(CellCorners,2);
+            fprintf(fileID, ['CELLS ' num2str(N_InternalFaces) ' ' num2str(N_InternalFaces*(1+N_NodePerFace)) '\n']);
+            
+            indMatrix = horzcat(N_NodePerFace*ones(N_InternalFaces,1) , CellCorners-1 );
+            
+            FormatSpec =[];
+            for n = 1 : N_NodePerFace
+                FormatSpec = strcat(FormatSpec,"%d ");
+            end
+            FormatSpec = char(FormatSpec);
+            FormatSpec(end)=[];
+            FormatSpec = strcat(FormatSpec,'\n');
+            fprintf(fileID, FormatSpec,indMatrix');
+            fprintf(fileID, '\n');
+            
+            fprintf(fileID, ['CELL_TYPES ' num2str(N_InternalFaces) '\n']);
+            fprintf(fileID, '%d ', 9*ones(1,N_InternalFaces));
+            fprintf(fileID, '\n\n');
+            
+            % Print all existing variables
+            fprintf(fileID, 'CELL_DATA %d\n', N_InternalFaces);
+            N_var = 1;
+            Names{1} = 'Trans';
+            for i=1:N_var
+                obj.PrintScalar2VTK(fileID, Grid.Trans, [' ',Names{i}]);
+                fprintf(fileID, '\n');
+            end
+
             fclose(fileID);
         end
     end
